56fb2a2f78bc6d36ee03ea716285176d7e403d0f
Jose Pacheco <jpacheco@csatf.org>
2017-03-16 14:50:21 -0700

    Add socket.io test
    Signed-off-by: Jose Pacheco <jpacheco@csatf.org>

--- web/app.js
+++ web/app.js
@@ -12,7 +12,11 @@ var usbScanner = require('node-usb-barcode-scanner').usbScanner;
 var getDevices = require('node-usb-barcode-scanner').getDevices;
 
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var port = 80;
+var scanner;
+var connectedHidDevices;
 
 app.use(timeout('15s'));
 app.use(router);
@@ -28,38 +32,57 @@ function logErrors (err, req, res, next) {
   next(err);
 }
 
-app.listen(port, function(request, response){
+io.on('connection', function(client){
+
+    console.log("CONNECTED");
+    console.log(io.sockets);
+
+    scanner.on("data", function(code){
+        console.log();
+        console.log("Activity");
+        console.log("-----------------");
+        console.log("Found Barcode: "+code);
+        client.emit('barcode', code);
+    });
+
+    // setTimeout(function(){
+    //     client.emit('barcode', 'BLAH');
+    // }, 500);
+
+});
+
+server.listen(port, function(request, response){
   
     console.log('Express server listening on port ' + port);
-    
+
     //get array of attached HID devices
-    var connectedHidDevices = getDevices();
-    
+    connectedHidDevices = getDevices();
+
     //print devices
     console.log(connectedHidDevices);
-    
+
     //initialize new usbScanner
-    var scanner = new usbScanner({
+    scanner = new usbScanner({
       vendorId : 4660
     });
-    
+
     //scanner emits a data event once a barcode has been read and parsed
-    scanner.on("data", function(code){
-      console.log();
-    	console.log("Activity");
-    	console.log("-----------------");
-      console.log("Found Barcode: "+code);
-      scanRequest.setBarcode(code);
-      scanRequest.respond();
-    });
-    
+    // scanner.on("data", function(code){
+    //   console.log();
+    // 	console.log("Activity");
+    // 	console.log("-----------------");
+    //   console.log("Found Barcode: "+code);
+    //   scanRequest.setBarcode(code);
+    //   scanRequest.respond();
+    // });
+
     // display scanRequest status
-    setInterval(function() {
-      console.log();
-    	console.log("Activity");
-    	console.log("-----------------");
-    	console.log("Client: "+( scanRequest.get() ? 'Connected' : 'Not connected'));
-    	
-    }, 2000);
+    // setInterval(function() {
+    //   console.log();
+    // 	console.log("Activity");
+    // 	console.log("-----------------");
+    // 	console.log("Client: "+( scanRequest.get() ? 'Connected' : 'Not connected'));
+    //
+    // }, 2000);
     
 });
